595db8c0dd14483609af50e6aea1c7e958ab501e
pseudomuto <david.muto@gmail.com>
2015-12-31 12:55:03 -0500

    updating to match flay settings

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,18 @@
 # rubocop:disable Style/SingleSpaceBeforeFirstArg
 name             "dude"
-maintainer       "Sweeper Devs"
+maintainer       "sweeper.io"
 maintainer_email "developers@sweeper.io"
 license          "mit"
 description      "Installs and configures dude"
 long_description "Installs and configures dude"
-version          "0.1.0"
+version          "0.1.1"
 # rubocop:enable Style/SingleSpaceBeforeFirstArg
 
-depends "apt"
-depends "runit"
+supports "ubuntu"
+
+depends "apt", "~> 2.0"
+depends "runit", "~> 1.0"
+
+chef_version ">= 12.5" if respond_to?(:chef_version)
+source_url "https://github.com/sweeperio/chef-dude" if respond_to?(:source_url)
+issues_url "https://github.com/sweeperio/chef-dude/issues" if respond_to?(:issues_url)
